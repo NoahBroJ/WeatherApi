b7baaa5c89305c6867adf5fa4c9870386d67ecda
NoahBroJ <noahzarkbro@gmail.com>
2017-08-30 14:25:24 +0200

    Uploaded to git

---
+++ WeatherApiCommunication/Client.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace WeatherApiCommunication
+{
+    public class Client
+    {
+        static HttpClient client = new HttpClient();
+        private static string apiKey = "32e7d04d86999102feb4439b0dc97b63";
+
+        public static async Task<WeatherData> RunAsync(string city)
+        {
+            client.BaseAddress = new Uri("http://localhost:55268/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            WeatherData data = null;
+
+            try
+            {
+                data = await GetProductAsync($"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            return data;
+        }
+
+        static async Task<WeatherData> GetProductAsync(string path)
+        {
+            WeatherData weatherData = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                weatherData = await response.Content.ReadAsAsync<WeatherData>();
+            }
+            return weatherData;
+        }
+    }
+}
---
+++ WeatherApiCommunication/WeatherData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApiCommunication
+{
+    public class WeatherData
+    {
+        private string name;
+        private string temp;
+
+        public string Name { get => name; }
+        public string Temp { get => temp; }
+
+        public WeatherData(string name, Dictionary<string, double> main)
+        {
+            this.name = name;
+            temp = main["temp"] - 273.15 + " °C";
+        }
+    }
+}
